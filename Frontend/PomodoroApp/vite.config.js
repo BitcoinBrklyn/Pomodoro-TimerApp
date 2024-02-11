0be59feda03c66bd0b220de7bcfaf6be45eee88d
BitcoinBrklyn <vincentmollins@rocketmail.com>
2024-02-10 21:26:08 -0500

    fixed errors

--- Frontend/PomodoroApp/vite.config.js
+++ Frontend/PomodoroApp/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // Set the base path to './' to prevent adding a leading slash
   plugins: [react()],
 });
